20329567c1d788420176216a3f7465982146f661
Wizzz01 <88884291+Wizzz01@users.noreply.github.com>
2024-09-17 16:43:12 +0800

    Update Data.sql

--- Data.sql
+++ Data.sql
@@ -1,6 +1,66 @@
---insert customers
+--Insert Customers
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (1, 'Yorke Shear', 'yshear0@51.la', '+51-325-895-7722', '97 Sullivan Drive');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (2, 'Hube Williamson', 'hwilliamson1@slideshare.net', '+966-111-386-0666', '06619 Roxbury Trail');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (3, 'Rosene Rebichon', 'rrebichon2@sun.com', '+86-224-689-5690', '48506 6th Park');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (4, 'Rufus Oven', 'roven3@istockphoto.com', '+33-664-270-0537', '7758 Hoffman Terrace');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (5, 'Gabriello Lorentzen', 'glorentzen4@weibo.com', '+86-127-565-2008', '247 Dunning Center');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (6, 'Glenden Kinge', 'gkinge5@blogspot.com', '+62-329-511-0319', '97286 Ohio Road');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (7, 'Shem Brownbill', 'sbrownbill6@oakley.com', '+62-123-552-3831', '58348 Waxwing Center');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (8, 'Aila Froud', 'afroud7@goo.gl', '+33-963-388-2898', '2256 Morning Circle');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (9, 'Devlen Kinsett', 'dkinsett8@about.com', '+1-630-528-2462', '99974 Stuart Plaza');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (10, 'Haley Houseago', 'hhouseago9@vinaora.com', '+62-261-815-0105', '42 Northview Road');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (11, 'Keen Bourke', 'kbourkea@hostgator.com', '+995-317-742-6880', '09669 Westerfield Place');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (12, 'Sella Todari', 'stodarib@pinterest.com', '+86-829-158-3552', '883 Morningstar Circle');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (13, 'Hendrik Autry', 'hautryc@newsvine.com', '+7-732-540-2361', '15 Moose Court');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (14, 'Humfried Dring', 'hdringd@opensource.org', '+380-690-294-4512', '927 Hazelcrest Place');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (15, 'Rena Dable', 'rdablee@earthlink.net', '+351-755-575-4424', '46326 Bowman Road');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (16, 'Pamella Kelsey', 'pkelseyf@myspace.com', '+86-905-957-7116', '76 Tomscot Pass');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (17, 'Darill Cancellieri', 'dcancellierig@twitter.com', '+1-567-707-2033', '87979 Sauthoff Plaza');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (18, 'Gregoire Moneypenny', 'gmoneypennyh@xinhuanet.com', '+351-669-386-7385', '363 Paget Trail');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (19, 'Teodor Mulroy', 'tmulroyi@nifty.com', '+48-248-839-5382', '6 Caliangt Street');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (20, 'Tyrus Alger', 'talgerj@google.co.uk', '+63-735-344-6191', '86 Harper Point');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (21, 'Sybilla Ellick', 'sellickk@angelfire.com', '+380-230-338-3662', '87 Sachtjen Parkway');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (22, 'Eleen Harbar', 'eharbarl@sitemeter.com', '+86-833-584-8311', '2868 Orin Place');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (23, 'Ibrahim Kelsell', 'ikelsellm@tripod.com', '+54-488-451-8924', '8 Oneill Terrace');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (24, 'Betsy Wissbey', 'bwissbeyn@cafepress.com', '+84-864-834-0207', '84 Forest Run Crossing');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (25, 'Nickie Bhar', 'nbharo@yellowpages.com', '+389-964-262-1506', '3781 Charing Cross Way');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (26, 'Millisent Mocquer', 'mmocquerp@dropbox.com', '+86-543-460-2474', '061 Esch Parkway');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (27, 'Maura Elloy', 'melloyq@bizjournals.com', '+7-201-182-6716', '977 Golf Course Street');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (28, 'Wat Antliff', 'wantliffr@time.com', '+251-966-488-4042', '9 Pankratz Place');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (29, 'Abdel Vignal', 'avignals@noaa.gov', '+509-430-832-2920', '83 Hanover Point');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (30, 'Elmira Leek', 'eleekt@senate.gov', '+34-420-532-1056', '4911 Mayer Alley');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (31, 'Ulysses Epple', 'ueppleu@state.tx.us', '+1-591-368-8792', '84 Village Green Pass');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (32, 'Izzy Bennedsen', 'ibennedsenv@t-online.de', '+62-703-862-3938', '44989 Comanche Road');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (33, 'Carlota Mulvany', 'cmulvanyw@japanpost.jp', '+47-790-257-0996', '4 Eastwood Point');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (34, 'Noll Carous', 'ncarousx@ocn.ne.jp', '+86-496-912-5662', '626 Melody Pass');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (35, 'Glory Johann', 'gjohanny@blog.com', '+62-120-243-2225', '78 Dexter Point');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (36, 'Fanchette Tinner', 'ftinnerz@squidoo.com', '+63-665-155-7188', '6714 Steensland Circle');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (37, 'Linell Placide', 'lplacide10@engadget.com', '+374-323-692-4082', '529 Pearson Circle');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (38, 'Welch Eddies', 'weddies11@narod.ru', '+7-191-941-3935', '4880 Rieder Way');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (39, 'Fernando Orto', 'forto12@slate.com', '+27-665-288-5801', '11307 Swallow Alley');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (40, 'Paule De La Coste', 'pde13@elpais.com', '+504-430-118-6675', '984 Melody Way');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (41, 'Ryon Huggill', 'rhuggill14@illinois.edu', '+36-325-264-0474', '8195 Dottie Crossing');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (42, 'Gratiana Maffini', 'gmaffini15@japanpost.jp', '+86-665-103-0001', '64 Hauk Lane');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (43, 'Wilbur Magenny', 'wmagenny16@tmall.com', '+63-616-336-2364', '5084 Huxley Park');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (44, 'Amalee Dadd', 'adadd17@usda.gov', '+420-945-716-6797', '22547 Forster Lane');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (45, 'Gayel Machans', 'gmachans18@webmd.com', '+7-450-846-6895', '84540 Stoughton Center');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (46, 'Mead Keightley', 'mkeightley19@cargocollective.com', '+7-738-912-9122', '12489 Corscot Point');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (47, 'Crissy Hawkswood', 'chawkswood1a@usa.gov', '+57-173-744-7322', '8608 Spenser Plaza');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (48, 'Alanson McQuin', 'amcquin1b@twitter.com', '+33-305-817-3202', '34 Cody Hill');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (49, 'Taite Glanister', 'tglanister1c@nsw.gov.au', '+355-871-612-3286', '56 Elgar Center');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (50, 'Joshua Burghall', 'jburghall1d@hexun.com', '+30-634-149-9384', '6 Crest Line Lane');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (51, 'Alia Vlasin', 'avlasin1e@meetup.com', '+351-191-986-8650', '57 Quincy Trail');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (52, 'Tana Matuschek', 'tmatuschek1f@economist.com', '+62-618-899-1728', '6974 Ryan Street');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (53, 'Marcy Wettern', 'mwettern1g@indiegogo.com', '+1-571-920-1310', '4760 Sutteridge Drive');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (54, 'Aundrea Cloake', 'acloake1h@prweb.com', '+62-439-675-9550', '5684 Amoth Avenue');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (55, 'Felicdad Chatters', 'fchatters1i@comcast.net', '+7-253-526-7684', '942 Gerald Trail');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (56, 'Fernando Ottley', 'fottley1j@loc.gov', '+86-105-299-8140', '34939 Manufacturers Lane');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (57, 'Neils Keighley', 'nkeighley1k@fda.gov', '+86-163-618-5032', '60872 Hanover Court');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (58, 'Doris Kingswood', 'dkingswood1l@github.io', '+55-102-270-5704', '44 Kennedy Parkway');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (59, 'Nonnah Caughte', 'ncaughte1m@guardian.co.uk', '+33-618-183-5723', '14118 Northview Avenue');
+insert into Customers (customer_id, customer_name, customer_email, customer_phone_number, customer_address) values (60, 'Guthrey Baylay', 'gbaylay1n@diigo.com', '+62-862-400-0787', '4 Magdeline Center');
 
---insert cars
+--Insert Cars
 INSERT INTO Cars (car_id, car_model, rental_price, movie_id)
 VALUES
 (1, '1995 Mitsubishi Eclipse', 250.00, 1),
@@ -48,7 +108,7 @@ VALUES
 (43, 'Citroën 2CV', 100.00, 9),
 (44, '1999 BMW Z8', 900.00, 10);
 
---insert movies
+--Insert Movies
 INSERT INTO Movies (movie_id, car_title, release_year, movie_rating)
 VALUES
 (1, 'The Fast and the Furious', '2001', 6.8),
@@ -61,3 +121,5 @@ VALUES
 (8, 'The Spy Who Loved Me', '1977', 7.0),
 (9, 'For Your Eyes Only', '1981', 6.7),
 (10, 'The World is Not Enough', '1999', 6.4);
+
+
